8172af8bbb43a2185e331d270c49a6925023ac55
lfacer <lfacer769@gmail.com>
2022-05-04 10:08:01 -0600

    first commit

---
+++ TicTacToe.cs
@@ -0,0 +1,111 @@
+using System;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          List<char> board = new List<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+           
+           DisplayBoard(board);
+
+           char currentPlayer = 'x';
+           int turn_count = 0;
+
+            while(!IsGameOver(board, turn_count))
+            {
+ 
+                Console.Write($"{currentPlayer}'s turn to choose a square: ");
+                
+                int choice = GetMoveChoice(currentPlayer);
+
+                MakeMove(board, choice, currentPlayer);
+                DisplayBoard(board);
+
+                currentPlayer = GetNextPlayer(currentPlayer);
+                turn_count += 1;
+            }
+        }
+
+        static void DisplayBoard(List<char> board)
+        {
+            Console.WriteLine(board[0] + "|" + board[1] + "|" + board[2]);
+            Console.WriteLine("-+-+-");
+            Console.WriteLine(board[3] + "|" + board[4] + "|" + board[5]);
+            Console.WriteLine("-+-+-");
+            Console.WriteLine(board[6] + "|" + board[7] + "|" + board[8]);
+        }
+        static char GetNextPlayer(char currentPlayer)
+        {
+            char NextPlayer = 'x';
+
+            if(currentPlayer == 'x')
+            {
+                NextPlayer = 'o';
+            }
+            return NextPlayer;
+        }
+        static int GetMoveChoice(char currentPlayer)
+        {
+            string move_string = Console.ReadLine();
+
+            int choice = int.Parse(move_string);
+            return choice;
+        }
+        static void MakeMove(List<char> board, int choice, char currentPlayer)
+        {
+            int index = choice - 1;
+
+            board[index] = currentPlayer;
+        }
+        static void UpdateBoard(List<char> board, char currentPlayer, int index)
+        {
+            board[index-1] = currentPlayer;
+        }
+        static bool DetermineWinner(List<char> board, char currentPlayer)
+        {
+            bool DetermineWinner = false;
+
+            if ((currentPlayer == board[0] && currentPlayer == board[1] && currentPlayer == board[2])
+            || (currentPlayer == board[3] && currentPlayer == board[4] && currentPlayer == board[5])
+            || (currentPlayer == board[6] && currentPlayer == board[7] && currentPlayer == board[8])
+            || (currentPlayer == board[0] && currentPlayer == board[3] && currentPlayer == board[6])
+            || (currentPlayer == board[1] && currentPlayer == board[4] && currentPlayer == board[7])
+            || (currentPlayer == board[2] && currentPlayer == board[5] && currentPlayer == board[8])
+            || (currentPlayer == board[0] && currentPlayer == board[4] && currentPlayer == board[8])
+            || (currentPlayer == board[2] && currentPlayer == board[4] && currentPlayer == board[6]))
+            {
+                DetermineWinner = true;
+                Console.WriteLine("Winner!");
+            }
+            return DetermineWinner;
+        }
+
+        static bool IsGameOver(List<char> board, int turn_count)
+        {
+            bool IsGameOver = false;
+            
+            if(DetermineWinner(board, 'x') || DetermineWinner(board, 'o') ||
+            IsTie(turn_count))
+            {
+                IsGameOver = true;
+            }
+
+            return IsGameOver;
+        }
+
+        static bool IsTie(int turn_count)
+        {
+           bool IsGameOver = false;
+
+           if(turn_count == 9)
+           {
+               IsGameOver = true;
+               Console.WriteLine("Tie!");
+           }
+           return IsGameOver;
+        }
+    
+    }
+}
